0f512645ec71117543b58f1c9d78317a6d32f215
gbl.huang <gbl403678@gaml.com>
2024-04-10 09:09:08 +0800

    init project

---
+++ src/main/java/com/mirror/ai/AiMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.mirror.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMirrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMirrorApplication.class, args);
+    }
+
+}
